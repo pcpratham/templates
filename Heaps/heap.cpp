2bc20ba87006e8237a1bfec68010e734a1ad9f9d
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-08-02 11:41:34 +0530

    heapify

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -65,22 +65,51 @@ class heap{
         }
 };
 
+void heapify(int arr[],int n,int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i + 1;
+
+    if(left<n && arr[largest] < arr[left]){
+        largest = left;
+    }
+    else if(right < n && arr[largest] < arr[right]){
+        largest = right;
+    }
+    
+    if(largest != i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
 int main(){
-    heap hs;
-    cout<<"heap statrted"<<endl;
-    hs.insert(60);
-    hs.insert(50);
-    hs.insert(40);
-    hs.insert(30);
-    hs.insert(20);
-    hs.insert(10);
+    // heap hs;
+    // cout<<"heap statrted"<<endl;
+    // hs.insert(60);
+    // hs.insert(50);
+    // hs.insert(40);
+    // hs.insert(30);
+    // hs.insert(20);
+    // hs.insert(10);
 
-    hs.print();
+    // hs.print();
 
-    hs.insert(55);
-    hs.print();
-    hs.deleteFromHeap();
-    hs.print();
+    // hs.insert(55);
+    // hs.print();
+    // hs.deleteFromHeap();
+    // hs.print();
+
+    int arr[6] = {-1,54,53,55,52,50};
+    int n = 5;
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+    cout<<"printing the array now "<<endl;
+    for(int i=1;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
